e908337ea24106a29caa2a8efbdb2f75b7df6267
TMiller00 <thomas@narmitech.com>
2025-01-16 11:10:58 -0500

    Buffer stdin

--- src/main.zig
+++ src/main.zig
@@ -74,8 +74,11 @@ fn readLine(reader: std.fs.File.Reader, allocator: std.mem.Allocator) ![]u8 {
     var line = std.ArrayList(u8).init(allocator);
     errdefer line.deinit();
 
+    var buf_reader = std.io.bufferedReader(reader);
+    var r = buf_reader.reader();
+
     while (true) {
-        if (reader.readByte()) |result| {
+        if (r.readByte()) |result| {
             if (result == '\n') break;
             try line.append(result);
         } else |err| {
