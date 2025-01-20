60409ea7995ce82a9e60e834e8b823703f957bac
TMiller00 <thomas@narmitech.com>
2025-01-20 00:17:27 -0500

    Consolidate launch and execute

--- src/execute.zig
+++ src/execute.zig
@@ -1,6 +1,34 @@
 const std = @import("std");
 const builtins = @import("builtins.zig");
-const launch = @import("launch.zig").launch;
+
+fn launch(args: [][]const u8, allocator: std.mem.Allocator) !c_int {
+    var child = std.process.Child.init(args, allocator);
+    try child.spawn();
+
+    const result = child.wait() catch {
+        std.debug.print("ish: command not found: {s}\n", .{args[0]});
+        return 1;
+    };
+
+    switch (result) {
+        .Exited => |code| {
+            if (code != 0) {
+                std.debug.print("Process exited with code {}\n", .{code});
+            }
+        },
+        .Signal => |signal| {
+            std.debug.print("Process terminated with signal {}\n", .{signal});
+        },
+        .Stopped => |code| {
+            std.debug.print("Process stopped with signal {}\n", .{code});
+        },
+        .Unknown => |code| {
+            std.debug.print("Process terminated with unknown {}\n", .{code});
+        },
+    }
+
+    return 1;
+}
 
 pub fn execute(args: [][]const u8, allocator: std.mem.Allocator) !c_int {
     if (args.len == 0) {
