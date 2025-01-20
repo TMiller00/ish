772a40357db79bf0960874606b6e8be2796b32e2
TMiller00 <thomas@narmitech.com>
2025-01-20 00:08:44 -0500

    Add commands

--- src/main.zig
+++ src/main.zig
@@ -1,69 +1,16 @@
 const std = @import("std");
-
-const CommandFn = *const fn (args: [][]const u8) u8;
-
-fn cd(args: [][]const u8) u8 {
-    if (args.len == 1) {
-        std.debug.print("ish: expected argument to \"cd\"\n", .{});
-        return 1;
-    }
-
-    if (args.len > 2) {
-        std.debug.print("cd: too many arguments\n", .{});
-        return 1;
-    }
-
-    _ = std.posix.chdir(args[1]) catch {
-        std.debug.print("cd: no such file or directory {s}\n", .{args[1]});
-    };
-
-    return 1;
-}
-
-fn exit(_: [][]const u8) u8 {
-    return 0;
-}
-
-const builtin_str = [2][]const u8{ "cd", "exit" };
-const builtin_fn = [2]CommandFn{ cd, exit };
-
-fn launch(args: [][]const u8, allocator: std.mem.Allocator) !c_int {
-    var child = std.process.Child.init(args, allocator);
-    try child.spawn();
-
-    const result = child.wait() catch {
-        std.debug.print("ish: command not found: {s}\n", .{args[0]});
-        return 1;
-    };
-
-    switch (result) {
-        .Exited => |code| {
-            if (code != 0) {
-                std.debug.print("Process exited with code {}\n", .{code});
-            }
-        },
-        .Signal => |signal| {
-            std.debug.print("Process terminated with signal {}\n", .{signal});
-        },
-        .Stopped => |code| {
-            std.debug.print("Process stopped with signal {}\n", .{code});
-        },
-        .Unknown => |code| {
-            std.debug.print("Process terminated with unknown {}\n", .{code});
-        },
-    }
-
-    return 1;
-}
+const builtins = @import("builtins.zig");
+const launch = @import("launch.zig").launch;
+const splitLine = @import("splitLine.zig").splitLine;
 
 fn execute(args: [][]const u8, allocator: std.mem.Allocator) !c_int {
     if (args.len == 0) {
         return 1;
     }
 
-    for (builtin_str, 0..) |builtin, index| {
+    for (builtins.builtin_str, 0..) |builtin, index| {
         if (std.mem.eql(u8, builtin, args[0])) {
-            return builtin_fn[index](args);
+            return builtins.builtin_fn[index](args);
         }
     }
 
@@ -93,18 +40,6 @@ fn readLine(reader: std.fs.File.Reader, allocator: std.mem.Allocator) ![]u8 {
     }
 
     return line.toOwnedSlice();
-}
-
-fn splitLine(line: []u8, allocator: std.mem.Allocator) ![][]const u8 {
-    var tokens = std.ArrayList([]const u8).init(allocator);
-    errdefer tokens.deinit();
-
-    var it = std.mem.tokenizeAny(u8, line, " \t\r\n");
-    while (it.next()) |token| {
-        try tokens.append(token);
-    }
-
-    return tokens.toOwnedSlice();
 }
 
 fn loop() !void {
