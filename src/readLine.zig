f5933adc79340748fa6c0c4471de2fd2e77bb4b1
TMiller00 <thomas@narmitech.com>
2025-01-20 18:25:11 -0500

    Use steamUntilDelimeter in readLine

--- src/readLine.zig
+++ src/readLine.zig
@@ -1,26 +1,13 @@
 const std = @import("std");
 
 pub fn readLine(reader: std.fs.File.Reader, allocator: std.mem.Allocator) ![]u8 {
-    var line = std.ArrayList(u8).init(allocator);
-    errdefer line.deinit();
-
     var buf_reader = std.io.bufferedReader(reader);
     var r = buf_reader.reader();
 
-    while (true) {
-        if (r.readByte()) |result| {
-            if (result == '\n') break;
-            try line.append(result);
-        } else |err| {
-            if (err == error.EndOfStream) {
-                if (line.items.len == 0) {
-                    return err;
-                }
-                break;
-            }
-            return err;
-        }
-    }
+    var line = std.ArrayList(u8).init(allocator);
+    errdefer line.deinit();
+
+    try r.streamUntilDelimiter(line.writer(), '\n', null);
 
     return line.toOwnedSlice();
 }
